2dfeae654c54799d1b6aa128eae9033359bc8a26
Selcuk ARSLAN <slckarslan93@gmail.com>
2023-01-04 20:45:42 +0300

    entity nullable

--- NLayerApp.Core/Product.cs
+++ NLayerApp.Core/Product.cs
@@ -1,18 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace NLayerApp.Core
+﻿namespace NLayerApp.Core
 {
     public class Product : BaseEntity
     {
-        public string Name { get; set; }
+        public string Name { get; set; } = string.Empty;
         public int Stock { get; set; }
         public int Price { get; set; }
         public int CategoryId { get; set; }
-        public Category Category { get; set; }
-        public ProductFeature ProductFeature { get; set; }
+        public Category? Category { get; set; }
+        public ProductFeature? ProductFeature { get; set; }
     }
 }
