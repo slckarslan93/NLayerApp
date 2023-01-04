5f44374c3f292bb77a280ecd367fa230207da91d
Selcuk ARSLAN <slckarslan93@gmail.com>
2023-01-04 20:59:45 +0300

    Generic Repository

---
+++ NLayerApp.Core/Repositories/IGenericRepository.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayerApp.Core.Repositories
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        Task<T> GetByIdAsync(int id);
+
+        IQueryable<T> GetAll(Expression<Func<T, bool>> expression);
+
+        IQueryable<T> Where(Expression<Func<T, bool>> expression);
+
+        Task<T> AnyAsync(Expression<Func<T, bool>> expression);
+
+        Task<T> AddAsync(T entity);
+
+        Task AddRangeAsync(IEnumerable<T> entities);
+
+        void Update(T entity);
+
+        void Remove(T entity);
+
+        void RemoveRange(IEnumerable<T> entities);
+    }
+}
