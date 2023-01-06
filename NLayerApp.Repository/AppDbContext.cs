e9edfc4ec7befce3d3c425c60749f9fdedc346cd
Selcuk ARSLAN <slckarslan93@gmail.com>
2023-01-06 22:20:42 +0300

    Seed Data

--- NLayerApp.Repository/AppDbContext.cs
+++ NLayerApp.Repository/AppDbContext.cs
@@ -22,6 +22,24 @@ namespace NLayerApp.Repository
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+            modelBuilder.Entity<ProductFeature>().HasData(new ProductFeature()
+            {
+                Id = 1,
+                Color = "Kırmızı",
+                Height = 100,
+                Width = 200,
+                ProductId = 1,
+            },
+            new ProductFeature()
+            {
+                Id = 2,
+                Color = "Mavi",
+                Height = 100,
+                Width = 200,
+                ProductId = 2,
+            });
+
             base.OnModelCreating(modelBuilder);
         }
     }
