710dc6644ba6d069e2d8546831c29a9ec8effb7e
Selcuk ARSLAN <slckarslan93@gmail.com>
2023-01-15 13:56:09 +0300

    NotFoundFilter

---
+++ NLayerApp.Web/NotFoundFilter.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using NLayerApp.Core.DTOs;
+using NLayerApp.Core.Model;
+using NLayerApp.Core.Services;
+
+namespace NLayerApp.Web
+{
+    public class NotFoundFilter<T> : IAsyncActionFilter where T : BaseEntity
+    {
+        private readonly IService<T> _service;
+
+        public NotFoundFilter(IService<T> service)
+        {
+            _service = service;
+        }
+
+        public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        {
+            var idValue = context.ActionArguments.Values.FirstOrDefault();
+            if (idValue == null)
+            {
+                await next.Invoke();
+                return;
+            }
+
+            var id = (int)idValue;
+            var enyEntity = await _service.AnyAsync(x => x.Id == id);
+            if (enyEntity)
+            {
+                await next.Invoke();
+            }
+            var errorViewModel = new ErrorViewModel();
+            errorViewModel.Errors.Add($"{typeof(T).Name}({id}) not found");
+            context.Result = new RedirectToActionResult("Error", "Home", errorViewModel);
+        }
+    }
+}
